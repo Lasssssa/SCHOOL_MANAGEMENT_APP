785d4def6b98f018a9523141dc6ef74c7ebc71b9
Lasssssa <theo.porodo@isen-ouest.yncrea.fr>
2023-06-09 20:17:42 +0200

    Adresse mail random

--- sql/data.sql
+++ sql/data.sql
@@ -7,15 +7,15 @@ INSERT INTO annee (numero_annee) VALUES ('2021-2022'),('2022-2023');
 INSERT INTO semestre (numero_semestre,id_annee) VALUES (1,2),(2,2),(1,1),(2,1);
 
 INSERT INTO administrateur (nom_admin,prenom_admin,telephone_admin,mail_admin,password_admin) VALUES 
-('Nourry','Cédric','0781908419','admin@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.');
+('Gachete','Vincent','0781900502','admintest@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.');
 
 INSERT INTO enseignant (nom_prof,prenom_prof,telephone_prof,password_prof,mail_prof) VALUES 
-('Karine','Ayoub','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','ayoub.karine@isen-ouest.yncrea.fr'),
-('Montero','Léandro','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','leandro.montero@isen-ouest.yncrea.fr'),
-('Lacourt','Marc','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','marc.lacourt@isen-ouest.yncrea.fr'),
-('Freixas','Jeremy','0785965748','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','jeremy.freixas@isen-ouest.yncrea.fr'),
-('Turmero','Virginia','1485963625','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','virginia.turmero@isen-ouest.yncrea.fr'),
-('Sorin','Matéo','0785965748','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','mateo.sorin@isen-ouest.yncrea.fr');
+('Michelet','Benoit','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','proftest@gmail.com'),
+('Menu','Jean','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','jean.menu@gmail.com'),
+('Trolea','Francois','0675859647','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','Francois.Trolea@gmail.com'),
+('Jean','Julien','0785965748','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','Julien.Jean@gmail.com'),
+('Hika','Salome','1485963625','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','Hika.Salome@gmail.com'),
+('Crono','Matéo','0785965748','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.','mateo.Crono@gmail.com');
 
 INSERT INTO cycle(nom_cycle) VALUES 
 ('CIR'),
@@ -53,23 +53,23 @@ INSERT INTO classe(annee_cursus,nom_cycle) VALUES
 (5,'MASTER');
 
 INSERT INTO etudiant (nom_etu,prenom_etu,mail_etu,password_etu,id_classe,telephone_etu) VALUES
-('Croguennoc','Romain','romain.croguennoc@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781963512'),
-('Pajdak','Antoine','antoine.pajdak@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0785363512'),
-('Porodo','Theo','theo.porodo@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781463512'),
-('Paitier','Mathias','mathias.paitier@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0725963512'),
-('Meunier','Mathis','mathis.meunier@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0621963512'),
-('Le Pan', 'Ethan', 'ethan.le-pan@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0736963512'),
-('Kebci','Paul','paul.kebci@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0782563512'),
-('Sermon','Goustan','goustan.sermon@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0712963512'),
-('Rena','Dorian','dorian.rena@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781963102'),
-('Rimbert','Sibylle','sibylle.rimbert@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0730963512'),
-('Vrain','Raphael','raphael.vrain@isne-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0720963512'),
-('Edet','Victor','victor.edet@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0781251412'),
-('Gadras','Erine','erine.gadras@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0781926512'),
-('Rabussier','Océane','oceane.rabussier@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781263512'),
-('Le Floch','Léonore','leonore.le-floch@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0681962512'),
-('Grimaud','Alex','alex.grimaud@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781963366'),
-('Petry','Guillaume','guillaume.petry@isen-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781965912');
+('Pidk','Romain','elevetest@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781963512'),
+('flute','Antoine','antoine.flute@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0785363512'),
+('Okia','Theo','theo.Okia@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781463512'),
+('maria','Mathias','mathias.maria@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0725963512'),
+('moinu','Mathis','mathis.moinu@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0621963512'),
+('Dupont', 'Ethan', 'ethan.Dupont@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0736963512'),
+('Kekeb','Paul','paul.Kekeb@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0782563512'),
+('Louia','Goustan','goustan.Louia@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0712963512'),
+('Renoat','Dorian','dorian.Renoat@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',8,'0781963102'),
+('Rimav','Sibylle','sibylle.Rimav@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0730963512'),
+('Sapo','Raphael','raphael.Sapo@isne-ouest.yncrea.fr','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0720963512'),
+('Edio','Victor','victor.Edio@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0781251412'),
+('Daoli','Erine','erine.Daoli@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',10,'0781926512'),
+('Issu','Océane','oceane.Issu@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781263512'),
+('Coach','Léonore','leonore.Coach@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0681962512'),
+('Lola','Alex','alex.Lola@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781963366'),
+('sesd','Guillaume','guillaume.sesd@gmail.com','$2y$10$fcTvVJwjFE73oS3elixLRux6zpnHh9Z66.Nefm.FLczB7YNLHSGv.',2,'0781965912');
 
 INSERT INTO cours (nom_matiere,duree,id_prof,id_semestre,id_classe) VALUES
 ('PHP CIR2', 60, 1, 2, 8),
